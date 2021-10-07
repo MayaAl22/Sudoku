054e0b4bc11450a062e32b139ba03f588dcca3cf
Maya Alroy <maya.alroy@code.berlin>
2021-10-07 17:39:11 +0200

    Added sudoku table and print function

---
+++ sudoku.py
@@ -0,0 +1,24 @@
+sudoku_table = [
+    [None, None, None, None, None, None, None, None, None], 
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None]
+    ]
+
+def print_grid():
+    for row in range(9):
+        for column in range(9):
+            if sudoku_table[row][column] == None:
+                print(".", end=" ")
+            else:
+                print(sudoku_table[row][column], end=" ")
+            if column == 2 or column == 5:
+                print("|", end=" ")
+        print("")
+        if row == 2 or row == 5:
+            print("------+-------+------")
