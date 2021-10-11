dc60022d60a3ea5a7ba108a82c1284550601c175
Maya Alroy <maya.alroy@code.berlin>
2021-10-11 14:29:48 +0200

    Add set number to table

--- sudoku.py
+++ sudoku.py
@@ -23,4 +23,39 @@ def print_grid():
         if row == 2 or row == 5:
             print("------+-------+------")
 
+def set_numbers_in_table():
+    sudoku_table[0][3] = 8
+    sudoku_table[0][5] = 7
+    sudoku_table[1][1] = 1
+    sudoku_table[1][4] = 9
+    sudoku_table[1][7] = 8
+    sudoku_table[2][3] = 3
+    sudoku_table[2][4] = 4
+    sudoku_table[2][5] = 1
+    sudoku_table[3][1] = 3
+    sudoku_table[3][2] = 8
+    sudoku_table[3][6] = 7
+    sudoku_table[3][7] = 9
+    sudoku_table[4][0] = 9
+    sudoku_table[4][8] = 1
+    sudoku_table[5][1] = 6
+    sudoku_table[5][2] = 1
+    sudoku_table[5][6] = 8
+    sudoku_table[5][7] = 4
+    sudoku_table[6][3] = 4
+    sudoku_table[6][4] = 5
+    sudoku_table[6][5] = 9
+    sudoku_table[7][0] = 7
+    sudoku_table[7][2] = 4
+    sudoku_table[7][3] = 1
+    sudoku_table[7][5] = 3
+    sudoku_table[7][6] = 6
+    sudoku_table[7][8] = 5
+    sudoku_table[8][1] = 8
+    sudoku_table[8][3] = 7
+    sudoku_table[8][5] = 2
+    sudoku_table[8][7] = 1
+
+set_numbers_in_table()
+
 print_grid()
