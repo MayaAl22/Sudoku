97753ccf4d6e063c2c15cda7c93226c5db7e1125
Maya Alroy <maya.alroy@code.berlin>
2021-11-08 17:07:03 +0100

    Added classes to represent the states of a field on the grid

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,24 @@
+class Preset:
+    def __init__(self, number):
+        self._value = number
+    
+    def get_value(self):
+        return self._value
+
+class Inserted:
+    def __init__(self, number):
+        self._value = number
+    
+    def get_value(self):
+        return self._value
+
+class Empty:
+    def __init__(self):
+        self._value = None
+    
+    def get_value(self):
+        return self._value
+
 class Grid:
     _region_size_width = 0
     _region_size_height = 0
@@ -19,81 +40,91 @@ class Grid:
         for row in range(size):
             self._grid.append([])
             for _ in range(size):
-                self._grid[row].append(None)
+                self._grid[row].append(Empty())
         self._set_values()
     
     def _set_values(self):
         if len(self._grid) == 4:
-            self._grid[0][0] = 3
-            self._grid[0][1] = 4
-            self._grid[0][2] = 1
-            self._grid[1][1] = 2
-            self._grid[2][2] = 2
-            self._grid[3][1] = 1
-            self._grid[3][2] = 4
-            self._grid[3][3] = 3
+            self._grid[0][0] = Preset(3)
+            self._grid[0][1] = Preset(4)
+            self._grid[0][2] = Preset(1)
+            self._grid[1][1] = Preset(2)
+            self._grid[2][2] = Preset(2)
+            self._grid[3][1] = Preset(1)
+            self._grid[3][2] = Preset(4)
+            self._grid[3][3] = Preset(3)
         elif len(self._grid) == 6:
-            self._grid[0][2] = 6
-            self._grid[0][4] = 3
-            self._grid[1][0] = 3
-            self._grid[1][1] = 1
-            self._grid[1][2] = 4
-            self._grid[1][4] = 2
-            self._grid[2][2] = 5
-            self._grid[2][3] = 1
-            self._grid[2][4] = 4
-            self._grid[2][5] = 3
-            self._grid[3][0] = 1
-            self._grid[3][1] = 4
-            self._grid[3][2] = 3
-            self._grid[3][3] = 2
-            self._grid[4][1] = 3
-            self._grid[4][3] = 5
-            self._grid[4][4] = 6
-            self._grid[4][5] = 2
-            self._grid[5][1] = 6
-            self._grid[5][3] = 3
+            self._grid[0][2] = Preset(6)
+            self._grid[0][4] = Preset(3)
+            self._grid[1][0] = Preset(3)
+            self._grid[1][1] = Preset(1)
+            self._grid[1][2] = Preset(4)
+            self._grid[1][4] = Preset(2)
+            self._grid[2][2] = Preset(5)
+            self._grid[2][3] = Preset(1)
+            self._grid[2][4] = Preset(4)
+            self._grid[2][5] = Preset(3)
+            self._grid[3][0] = Preset(1)
+            self._grid[3][1] = Preset(4)
+            self._grid[3][2] = Preset(3)
+            self._grid[3][3] = Preset(2)
+            self._grid[4][1] = Preset(3)
+            self._grid[4][3] = Preset(5)
+            self._grid[4][4] = Preset(6)
+            self._grid[4][5] = Preset(2)
+            self._grid[5][1] = Preset(6)
+            self._grid[5][3] = Preset(3)
         elif len(self._grid) == 9:
-            self._grid[0][3] = 8
-            self._grid[0][5] = 7
-            self._grid[1][1] = 1
-            self._grid[1][4] = 9
-            self._grid[1][7] = 8
-            self._grid[2][3] = 3
-            self._grid[2][4] = 4
-            self._grid[2][5] = 1
-            self._grid[3][1] = 3
-            self._grid[3][2] = 8
-            self._grid[3][6] = 7
-            self._grid[3][7] = 9
-            self._grid[4][0] = 9
-            self._grid[4][8] = 1
-            self._grid[5][1] = 6
-            self._grid[5][2] = 1
-            self._grid[5][6] = 8
-            self._grid[5][7] = 4
-            self._grid[6][3] = 4
-            self._grid[6][4] = 5
-            self._grid[6][5] = 9
-            self._grid[7][0] = 7
-            self._grid[7][2] = 4
-            self._grid[7][3] = 1
-            self._grid[7][5] = 3
-            self._grid[7][6] = 6
-            self._grid[7][8] = 5
-            self._grid[8][1] = 8
-            self._grid[8][3] = 7
-            self._grid[8][5] = 2
-            self._grid[8][7] = 1
+            self._grid[0][3] = Preset(8)
+            self._grid[0][5] = Preset(7)
+            self._grid[1][1] = Preset(1)
+            self._grid[1][4] = Preset(9)
+            self._grid[1][7] = Preset(8)
+            self._grid[2][3] = Preset(3)
+            self._grid[2][4] = Preset(4)
+            self._grid[2][5] = Preset(1)
+            self._grid[3][1] = Preset(3)
+            self._grid[3][2] = Preset(8)
+            self._grid[3][6] = Preset(7)
+            self._grid[3][7] = Preset(9)
+            self._grid[4][0] = Preset(9)
+            self._grid[4][8] = Preset(1)
+            self._grid[5][1] = Preset(6)
+            self._grid[5][2] = Preset(1)
+            self._grid[5][6] = Preset(8)
+            self._grid[5][7] = Preset(4)
+            self._grid[6][3] = Preset(4)
+            self._grid[6][4] = Preset(5)
+            self._grid[6][5] = Preset(9)
+            self._grid[7][0] = Preset(7)
+            self._grid[7][2] = Preset(4)
+            self._grid[7][3] = Preset(1)
+            self._grid[7][5] = Preset(3)
+            self._grid[7][6] = Preset(6)
+            self._grid[7][8] = Preset(5)
+            self._grid[8][1] = Preset(8)
+            self._grid[8][3] = Preset(7)
+            self._grid[8][5] = Preset(2)
+            self._grid[8][7] = Preset(1)
+    
+    def _number_in_row_column(self, row, column, number):
+        # check row
+        if number in [ field.get_value() for field in self._grid[row] ]:
+            return True
+        # check column
+        elif number in [ row[column].get_value() for row in self._grid ]:
+            return True
+        else:
+            return False
     
     def print(self):
         for row in range(len(self._grid)):
             for column in range(len(self._grid[row])):
                 # print value
-                if self._grid[row][column] == None:
+                if isinstance(self._grid[row][column], Empty):
                     print(".", end=" ")
                 else:
-                    print(self._grid[row][column], end=" ")
+                    print(self._grid[row][column].get_value(), end=" ")
                 # print vertical line
                 if column > 0 and column < len(self._grid[row])-1 and (column+1) % self._region_size_width == 0:
                     print("|", end=" ")
@@ -103,24 +134,23 @@ class Grid:
                 print("--" * (len(self._grid) + int(len(self._grid) / self._region_size_width) - 1))
 
     def erase_field(self, row, column):
-        self._grid[row][column] = None
+        if isinstance(self._grid[row][column], Preset):
+            print("Field has preset number")
+        elif isinstance(self._grid[row][column], Empty):
+            print("Field is empty")
+        else:
+            self._grid[row][column] = Empty()
 
     def set_field(self, row, column, number):
-        sudoku_table_column = []
-        for loop_row in self._grid:
-            sudoku_table_column.append(loop_row[column])
-
-        if number in self._grid[row]:
-            print("Number already exists in row")
-        elif number in sudoku_table_column:
-            print("Number already exists in column")
+        if self._number_in_row_column(row, column, number):
+            print("Number already exists in row or column")
         else:
-            self._grid[row][column] = number
+            self._grid[row][column] = Inserted(number)
         
     def is_won(self):
         for row in self._grid:
-            for column in row:
-                if column == None:
+            for field in row:
+                if isinstance(field, Empty):
                     return False
         return True
 
@@ -170,4 +200,3 @@ while True:
         print("Game won!")
         break
     my_grid.print()
-    
